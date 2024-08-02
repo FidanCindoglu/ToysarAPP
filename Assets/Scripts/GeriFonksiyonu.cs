eec961e6091e53d53b57f2f6c97fc051dcd16f5e
Fidan Cindoğlu <123310124+FidanCindoglu@users.noreply.github.com>
2024-08-02 11:24:59 +0300

    Add files via upload

---
+++ Assets/Scripts/GeriFonksiyonu.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GeriFonksiyonu : MonoBehaviour
+{
+    public void MenuSayfasi () {
+        SceneManager.LoadScene("MenuEkrani2");
+        
+    }
+}
---
+++ Assets/Scripts/MisafirKullanici.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MisafirKullanici : MonoBehaviour
+{
+    public void ARCAMScene () {
+        SceneManager.LoadScene("ARCAM3");
+        
+    }
+}
